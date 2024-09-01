a2817af2921b1f2815d350972e21278ad8d553de
portfolioaidiladam <portfolioaidiladam@gmail.com>
2024-09-01 16:19:32 +0700

    commit for update belajar restfull api detailnya Address api spec, Search contact api, Adress Entity, Update User api

---
+++ src/main/java/testmsigdeveloperBEaidil/restful/entity/Address.java
@@ -0,0 +1,40 @@
+package testmsigdeveloperBEaidil.restful.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "addresses")
+public class Address {
+
+    @Id
+    private String id;
+
+    private String street;
+
+    private String city;
+
+    private String province;
+
+    private String country;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_id", referencedColumnName = "id")
+    private Contact contact;
+
+    /*@OneToMany (mappedBy = "contact")
+    private List <Address> addresses;*/
+
+}
